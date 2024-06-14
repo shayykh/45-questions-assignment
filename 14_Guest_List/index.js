aae58f802fbcc1adfa829942a7de05ea513f6b66
shayykh <shayanshaikh2345@gmail.com>
2024-06-14 23:25:20 +0500

    Q11-15 completed

---
+++ 14_Guest_List/index.js
@@ -0,0 +1,10 @@
+//14. Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+let guests = ['Tariq', 'Jinnah', 'Iqbal', 'Quaid'];
+for (let i = 0; i < guests.length; i++) {
+    console.log(`Dear ${guests[i]}, you are cordially invited to dinner.`);
+}
+export {};
+//Alternate Solution:
+//let guests: string[] = ['Tariq', 'Jinnah', 'Iqbal', 'Quaid'];
+//guests.forEach(guest => console.log(`Dear ${guest}, you are cordially invited to dinner.`)); 
+//ForEach method 
---
+++ 15_Changing_GuestList/index.ts
@@ -0,0 +1,24 @@
+//15. Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+// • Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+// • Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+// • Print a second set of invitation messages, one for each person who is still in your list.
+
+
+//Program 14
+let guests: string[] = ['Tariq', 'Jinnah', 'Iqbal', 'Quaid'];
+
+for (let i = 0; i < guests.length; i++) {
+    console.log(`Dear ${guests[i]}, you are cordially invited to dinner.`);
+}
+
+//Program 15
+let cantAttend: string = 'Jinnah';
+
+console.log(`\nUnfortunately, ${cantAttend} can't make it to the dinner.\n`);
+
+let newGuest: string = 'Rehmat Ali';
+guests[guests.indexOf(cantAttend)] = newGuest;
+
+guests.forEach(guest => {
+    console.log(`Dear ${guest}, you are cordially invited to dinner.`);
+});
