023bf7e39e4009ba7babb21f26412c3391aa8192
shayykh <shayanshaikh2345@gmail.com>
2024-06-19 20:43:49 +0500

    Q34,35 formatted

--- 34_Pizzas/index.ts
+++ 34_Pizzas/index.ts
@@ -2,10 +2,10 @@
 // • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
 // • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
 
-const pizzas = ['Pepperoni', 'Cheese', 'Afghani'];
+const pizzas = ["Pepperoni", "Cheese", "Afghani"];
 
 for (let i = 0; i < pizzas.length; i++) {
-    console.log(`I like ${pizzas[i]} pizza`);
+  console.log(`I like ${pizzas[i]} pizza`);
 }
 
 //Alternative way
@@ -13,5 +13,4 @@ for (let i = 0; i < pizzas.length; i++) {
 //     console.log(`I like ${pizza} pizza`);
 // });
 
-console.log('I really love pizza!');
-
+console.log("I really love pizza!");
